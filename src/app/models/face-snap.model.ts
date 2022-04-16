f447affeecea6ed0a756a84538847400305940a8
Ilyes Bt <ilyes.bettaieb5@gmail.com>
2022-04-16 03:19:51 +0100

    first commit

---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,12 @@
+export class FaceSnap{
+    
+         title!: string;
+         description!:string;
+         imageUrl!:string;
+         createDate!: Date;
+         snaps!: number;
+         location?: string
+         opt?: string;
+         id!:number;
+    
+}
